4ed595df5a94d91d4100b4334658af05a0da7275
Alexey Noskov <alexey.noskov@gmail.com>
2014-12-18 11:41:16 +0300

    Add cache version

--- lib/cache/exts/rails_cache_store.rb
+++ lib/cache/exts/rails_cache_store.rb
@@ -23,8 +23,17 @@ module Cache
         @cache_name ||= self.class.name.underscore
       end
 
+      def cache_version
+        @cache_version ||= \
+          if self.class.const_defined?(:CACHE_VERSION)
+            self.class.const_get(:CACHE_VERSION)
+          else
+            0
+          end
+      end
+
       def transform_cache_key(key)
-        super [cache_name, key]
+        super [cache_name, cache_version, key]
       end
 
     end
